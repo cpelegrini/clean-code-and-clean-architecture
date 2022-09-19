a967e36c87a64733770e97aa0976ac95f3c342ef
Claudinei Pelegrini <claudineipelegrini@hotmail.com>
2022-09-19 07:44:32 -0300

    improvments

--- project/test/Cpf.test.ts
+++ project/test/Cpf.test.ts
@@ -1,36 +1,32 @@
-import Cpf  from "../src/Cpf";
-describe('Cpf', ()=>{
-;
-    test("Should validate cpf with mask", function () {
-        expect(new Cpf().isValid('111.444.777-35')).toBeTruthy();
-    });
-    
-    test("Should validate cpf with no mask", function () {
-        expect(new Cpf().isValid('11144477735')).toBeTruthy();
-    });
+import Cpf from "../src/Cpf";
+describe("Cpf", () => {
+   test("Should validate cpf with mask", function () {
+      expect(new Cpf().isValid("111.444.777-35")).toBeTruthy();
+   });
 
-    test("Should not validate cpf with strang char", function () {
-        expect(new Cpf().isValid('a97.067.080-00')).toBeFalsy()
-    });
+   test("Should validate cpf with no mask", function () {
+      expect(new Cpf().isValid("11144477735")).toBeTruthy();
+   });
 
-    test("Should not validate undefined cpf", function () {
-        expect(new Cpf().isValid('')).toBeFalsy()
-    });
+   test("Should not validate cpf with strang char", function () {
+      expect(new Cpf().isValid("a97.067.080-00")).toBeFalsy();
+   });
 
+   test("Should not validate undefined cpf", function () {
+      expect(new Cpf().isValid("")).toBeFalsy();
+   });
 
    test("Should not validate invalid size cpf", function () {
-        expect(new Cpf().isValid('1231231231231')).toBeFalsy()
-    });
+      expect(new Cpf().isValid("1231231231231")).toBeFalsy();
+   });
 
-    test("Should not validate cpf repeated same number", function () {
-        expect(new Cpf().isValid('111.111.111-11')).toBeFalsy();
-    });
+   test("Should not validate cpf repeated same number", function () {
+      expect(new Cpf().isValid("111.111.111-11")).toBeFalsy();
+   });
 
-    test("Should generate 10 cpf and validate", function () {
-        const cpf = new Cpf();
-        const generated = cpf.generate();
-        expect(cpf.isValid(generated)).toBeTruthy();
-    });
-
-})
-
+   test("Should generate 10 cpf and validate", function () {
+      const cpf = new Cpf();
+      const generated = cpf.generate();
+      expect(cpf.isValid(generated)).toBeTruthy();
+   });
+});
